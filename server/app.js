ff9536467bdee97390f5f756c256de412cdd0faa
raphaelkhan8 <40365951+raphaelkhan8@users.noreply.github.com>
2019-11-02 13:56:51 -0500

    made port dynamic for deployment

--- server/app.js
+++ server/app.js
@@ -669,7 +669,7 @@ app.get('*', (req, res) => {
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`you servin on port ${port}`);
 });
